d22c99034008886047334147720fb7c9f3b271e9
Alexis Craye <alexis1.craye@epitech.eu>
2020-09-26 10:35:15 +0200

    adding lauchfile to wall follower

--- wall_follower/launch/wall_follower.launch.py
+++ wall_follower/launch/wall_follower.launch.py
@@ -15,16 +15,24 @@ TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    # world_file_name = 'turtlebot3_worlds/' + TURTLEBOT3_MODEL + '.model'
+    world_file_name = 'custom_worlds/' + TURTLEBOT3_MODEL + '.world'
+    world_file_name = 'custom_worlds/' + 'challenge_maze' + '.world'
+    world = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'worlds', world_file_name)
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
 
     return LaunchDescription([
+        # ExecuteProcess(
+        #     cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so'],
+        #     output='screen'),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([launch_file_dir, '/robot_state_publisher.launch.py']),
             launch_arguments={'use_sim_time': use_sim_time}.items(),
         ),
         Node(
-            package='wall_follower',
-            executable='wall_follower',
-            name='wall_follower'
+            package='py_stupidobstacleavoidance',
+            executable='the_smartest_bot_i_ve_ever_seen',
+            name='my_smart_bot'
         )
     ])
