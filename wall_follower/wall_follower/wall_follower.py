26c3b243f91e9ea349ce559f50463b6ed8de0279
Alexis Craye <alexis1.craye@epitech.eu>
2020-10-12 16:05:34 +0200

    update wall following algorithm. Bot can exit the maze!

--- wall_follower/wall_follower/wall_follower.py
+++ wall_follower/wall_follower/wall_follower.py
@@ -17,6 +17,7 @@ class WallFollower(Node):
     STATE_SEARCH_WALL = 0
     STATE_MOVE_TO_WALL = 1
     STATE_FOLLOW_WALL = 2
+    STATE_TEST_DEPLACEMENT = 3
 
     def __init__(self):
         super().__init__('wall_follower')
@@ -45,6 +46,8 @@ class WallFollower(Node):
         self._cur_linear = self.MIN_LINEAR
         self._cur_angular = self.MIN_ANGULAR
         self._cur_state = self.STATE_SEARCH_WALL
+        # self._cur_state = self.STATE_TEST_DEPLACEMENT
+        self._last_best_wall_direction = None
 
         self._safety_distance = 0.3
 
@@ -182,6 +185,10 @@ class WallFollower(Node):
         if self._current_ranges.length < self._current_ranges.max_size:
             return
         walls = self.__get_walls()
+        self.info(f'Found {len(walls)} walls.')
+        for w in walls:
+            self.info(f'{w.closer_dist_from_origin()}')
+        # exit(1)
         best_wall = self.__get_closer_wall(walls)
         self.__reset_location_and_rotation()
         self._target_location = best_wall.closer_point_from_origin() + (rotate_vector(best_wall.direction, -90) * self._safety_distance)
@@ -191,6 +198,7 @@ class WallFollower(Node):
 
     def __move_to_wall(self):
         if self._rotation != self._target_rotation:
+            # self.info(f'ROTATE -- {self._cur_linear} -- {self._cur_angular}')
             diff = abs(self._target_rotation - self._rotation)
             move = self.MAX_ANGULAR * get_sign(self._target_rotation)
             next_rotation = move * self.update_delay
@@ -201,6 +209,7 @@ class WallFollower(Node):
             self._cur_linear = self.MIN_LINEAR
             self._rotation += next_rotation
         elif self._location != self._target_location:
+            # self.info(f'GO FORWARD -- {self._cur_linear} -- {self._cur_angular}')
             diff = self._target_location - self._location
             move = self.MAX_LINEAR
             next_movement = diff.normalized * move * self.update_delay
@@ -211,10 +220,12 @@ class WallFollower(Node):
             self._cur_linear = move
             self._location += next_movement
         else:
+            # self.info(f'FINISH! -- {self._cur_linear} -- {self._cur_angular}')
             self._cur_angular = self.MIN_ANGULAR
             self._cur_linear = self.MIN_LINEAR
             self.__log_change_state(self._cur_state, self.STATE_FOLLOW_WALL)
             self._cur_state = self.STATE_FOLLOW_WALL
+            # self._cur_state = self.STATE_TEST_DEPLACEMENT
             self._current_ranges.clear()
 
     def __find_next_point(self, best_wall: Wall, walls: list):
@@ -227,16 +238,26 @@ class WallFollower(Node):
             best_wall_extremity = best_wall.end_point
         best_direction = rotate_vector(best_wall.direction, -90)
         next_point = best_wall_extremity + (best_direction * self._safety_distance)
-        if next_point.dist > 8:
-            next_point = next_point.normalized * 8
+        if next_point.dist > 1:
+            next_point = next_point.normalized * 1
         for wall in (w for w in walls if w != best_wall):
             intersection = get_intersection(Point.zero(), best_wall.direction.normalized, wall.begin_point,
                                             wall.direction)
-            if next_point.dist + self._safety_distance > intersection.dist:
+            intersection_angle = get_angle(intersection)
+            next_point_angle = get_angle(next_point)
+            angles_diff = abs(intersection_angle - next_point_angle)
+            if angles_diff > 180:
+                angles_diff = 360 - angles_diff
+            from_begin_dist = (intersection - wall.begin_point).dist
+            from_end_dist = (intersection - wall.end_point).dist
+            self.info(f'{intersection_angle} -- {next_point_angle} -- {angles_diff} -- {from_begin_dist} -- {from_end_dist}')
+            if next_point.dist + self._safety_distance > intersection.dist and angles_diff < 45 and from_begin_dist < wall.dist and from_end_dist < wall.dist:
+                self.info(f'Need recusrivity {wall} -- {intersection} -- {(intersection - wall.begin_point).dist} -- {(intersection - wall.end_point).dist}\n-- {(next_point - intersection).dist} -- {get_angle(next_point)} -- {get_angle(intersection)}')
                 return self.__find_next_point(wall, [w for w in walls if w != best_wall])
         if next_point.dist < 0.1:
             self.info(f'Next point to close: {next_point.dist}')
-            next_point = best_wall.end_point + (best_wall.direction * self._safety_distance)
+            next_point = best_wall.end_point + (rotate_vector(best_wall.direction, 30) * self._safety_distance)
+            self._last_best_wall_direction = best_wall.direction
         else:
             self.info(f'Next point: {next_point.dist}')
         return next_point
@@ -246,9 +267,13 @@ class WallFollower(Node):
         self._cur_angular = self.MIN_ANGULAR
         if self._current_ranges.length < self._current_ranges.max_size:
             return
-        self.__reset_location_and_rotation()
         walls = self.__get_walls()
         self.get_logger().info(f'Walls count: {len(walls)}')
+        for w in walls:
+            self.info(f'{w.closer_dist_from_origin()} -- {get_angle(w.direction)} -- {w.dist}')
+        if self._last_best_wall_direction is not None:
+            self.info(f'After turning -> {get_angle(self._last_best_wall_direction)} -- {math.degrees(self._target_rotation)}')
+        self.__reset_location_and_rotation()
         best_wall = self.__get_closer_wall(walls)
         next_point = self.__find_next_point(best_wall, walls)
         self.get_logger().info(f'NEXT POINT {next_point}')
@@ -257,10 +282,18 @@ class WallFollower(Node):
         self.__log_change_state(self._cur_state, self.STATE_MOVE_TO_WALL)
         self._cur_state = self.STATE_MOVE_TO_WALL
 
+    def __test_deplacement(self):
+        self.__reset_location_and_rotation()
+        self._target_location = Point.up() * 4
+        self._target_rotation = math.radians(get_angle(self._target_location))
+        # self.info(f'TEST: {self._target_location} -- {self._target_rotation}')
+        self._cur_state = self.STATE_MOVE_TO_WALL
+
     def __publish(self):
         twist = Twist()
         twist.linear.x = self._cur_linear
         twist.angular.z = self._cur_angular
+        # self.info(f'{twist}')
         self.publisher.publish(twist)
 
     def scan_sensor_callback(self, sensor_data):
@@ -268,7 +301,10 @@ class WallFollower(Node):
         self._current_ranges.append(sensor_data.ranges)
 
     def odom_callback(self, odometry):
-        # self.get_logger().info(f'CMD VEL ROW -> {odometry}')
+        twist = odometry.twist.twist
+        linear = twist.linear
+        angular = twist.angular
+        # self.get_logger().info(f'ODOM\n{linear.x} -- {linear.y}\n{angular.z}')
         return
 
     def update_callback(self):
@@ -279,7 +315,8 @@ class WallFollower(Node):
     STATES = {
         STATE_SEARCH_WALL: __search_for_wall,
         STATE_MOVE_TO_WALL: __move_to_wall,
-        STATE_FOLLOW_WALL: __follow_wall
+        STATE_FOLLOW_WALL: __follow_wall,
+        STATE_TEST_DEPLACEMENT: __test_deplacement
     }
 
 
