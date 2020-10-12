10177782ab3d1cbfad3a4a96f077e44ca6d80eae
Alexis Craye <alexis1.craye@epitech.eu>
2020-10-12 23:18:57 +0200

    adding environment into launch file

--- wall_follower/launch/start_map.launch.py
+++ wall_follower/launch/start_map.launch.py
@@ -1,12 +1,19 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+import os
 
 
 def generate_launch_description():
     world = 'src/worlds/blackfoot_world/worlds/challenge_maze.burger.world'
+    env = os.environ
+    env['GAZEBO_MODEL_PATH'] = ':'.join(('src/worlds/blackfoot_world/models',
+                                         'src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models',
+                                         env.setdefault('GAZEBO_MODEL_PATH', '')))
 
     return LaunchDescription([
         ExecuteProcess(
             cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so'],
-            output='screen'),
+            output='screen',
+            env=env
+        ),
     ])
