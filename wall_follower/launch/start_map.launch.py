87d0d1c516492f1e0bb6564ee2d51fbaa20ae75b
MonkeYKonG <MonkeYKonG@users.noreply.github.com>
2020-10-29 23:38:48 +0100

    Update start_map.launch.py

--- wall_follower/launch/start_map.launch.py
+++ wall_follower/launch/start_map.launch.py
@@ -4,10 +4,10 @@ import os
 
 
 def generate_launch_description():
-    world = 'src/custom/smart_bot_driver/worlds/challenge_maze.burger.world'
+    world = 'custom/smart_bot_driver/worlds/challenge_maze.burger.world'
     env = os.environ
-    env['GAZEBO_MODEL_PATH'] = ':'.join(('src/custom/smart_bot_driver/models',
-                                         'src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models',
+    env['GAZEBO_MODEL_PATH'] = ':'.join(('custom/smart_bot_driver/models',
+                                         'turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models',
                                          env.setdefault('GAZEBO_MODEL_PATH', '')))
 
     return LaunchDescription([
